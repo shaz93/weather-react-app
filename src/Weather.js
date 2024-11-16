afb5c8661ad781da6a02f3f519ffd97f531fd63e
shaz93 <sheronndimande93@gmail.com>
2024-11-16 23:46:53 +0200

    added weather css

--- src/Weather.js
+++ src/Weather.js
@@ -1,33 +1,41 @@
 import React from "react";
-
+import "./Weather";
 
 export default function Weather() {
-    return (
-        <div className="Weather">
-            <form>
-                <input type="search"placeholder="Enter a city..."className="form-control"/>
-                <input  type="submit"value="search"/>
-            </form>
-            <h1>New York</h1>
-            <ul>
-                <li>
-                    Wednesday 7:00
-                </li>
-                <li>Mostly Cloudy</li>
-            </ul>
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"alt="Mostly Cloudy "/>
-                    6°C
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Precipitation:15%</li>
-                        <li>Humidity:72</li>
-                        <li>Wind:13km/hr</li>
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          className="form-control"
+          autoFocus="on"
+        />
+        <input type="submit" value="search" />
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Mostly Cloudy "
+            className="float-left"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C </span>
         </div>
-    )
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:15%</li>
+            <li>Humidity:72</li>
+            <li>Wind:13km/hr</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
