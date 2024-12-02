9e532c416e1d7a8af00a1b942717ee727dd60e94
shaz93 <sheronndimande93@gmail.com>
2024-12-02 07:29:16 +0200

    updated netlify link

--- src/App.js
+++ src/App.js
@@ -9,18 +9,32 @@ export default function App() {
         <Weather defaultCity="London" />
 
         <footer>
-          This project was coded by {" "}
-          <a href="https://github.com/" target="blank" rel="noopener noreferrer">
+          This project was coded by{" "}
+          <a
+            href="https://github.com/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
             {" "}
             Sheron Ndimande
-          </a>
-          {" "} and is open-sourced on {" "}
-          <a href="https://github.com/shaz93/react-weather" target="blank" rel="noopener noreferrer">
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/shaz93/react-weather"
+            target="blank"
+            rel="noopener noreferrer"
+          >
             {" "}
             Github
+          </a>{" "}
+          , hosted on{" "}
+          <a
+            href="https://joyful-unicorn-61a505.netlify.app"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
           </a>
-         {" "}, hosted on {" "}
-          <a href="/" target="blank" rel="noopener noreferrer">Netlify</a>
         </footer>
       </div>
     </div>
